b8bc6692cd05f3087832d10e8e023ddd70a04f96
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2016-10-29 19:16:16 -0300

    Correções gerais

--- Banco.php
+++ Banco.php
@@ -1,12 +1,12 @@
 <?php
 abstract class Banco {
 
-    private $dsn = "mysql:host=localhost;dbname=terceiro";
-    private $usuario = "root";
-    private $senha = "";
+    private static $dsn = "mysql:host=localhost;dbname=terceiro";
+    private static $usuario = "root";
+    private static $senha = "";
 
     public static function getInstance() {
-        $con = new PDO($dsn, $usuario, $senha);
+        $con = new PDO(self::$dsn, self::$usuario, self::$senha);
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $con->exec("SET CHARACTER SET utf8");
         return $con;
